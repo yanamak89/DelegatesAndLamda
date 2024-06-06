b25691dbecbf118c5a3971c5dcca48d3a28e4150
Yana Makogon <yana.mak89@gmail.com>
2024-06-06 21:17:31 +0200

    Task3 completed

---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+delegate int RandomValueDelegate();
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        
+        // Створення масиву делегатів
+        RandomValueDelegate[] delegates = new RandomValueDelegate[5];
+        for (int i = 0; i < delegates.Length; i++)
+        {
+            // Ініціалізація кожного делегата анонімним методом, який повертає випадкове значення
+            delegates[i] = () => random.Next(1, 101); // Випадкове значення від 1 до 100
+        }
+
+        // Анонімний метод для обчислення середнього арифметичного
+        Func<RandomValueDelegate[], double> calculateAverage = delegate (RandomValueDelegate[] delArray)
+        {
+            double sum = 0;
+            foreach (var del in delArray)
+            {
+                sum += del();
+            }
+
+            return sum / delArray.Length;
+        };
+
+        // Виклик анонімного методу та виведення результату
+        double average = calculateAverage(delegates);
+        Console.WriteLine($"The average value is: {average}");
+        Console.ReadKey();
+    }
+}
